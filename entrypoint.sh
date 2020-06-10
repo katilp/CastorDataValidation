635f63f321606fa29966be9be57ef28bf7849949
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2020-06-10 18:57:08 +0200

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -l
+
+cd /home/cmsusr
+
+set  -e
+
+echo "Setting up CMSSSW_4_2_8"
+source /opt/cms/cmsset_default.sh
+scramv1 project CMSSW CMSSW_4_2_8
+cd CMSSW_4_2_8/src
+eval `scramv1 runtime -sh`
+echo "CMSSW_4_2_8 is at your service."
+
+
+git clone https://github.com/cms-legacydata-validation/CastorDataValidation.git
+ls -l
+#scram b
+
+#edmProvDump --sort $1 > dump.txt
+#sudo chown -R cmsusr /github/workspace
+#chmod 755 /github/workspace
+#cp dump.txt /github/workspace
+
+
+echo "::set-output name=another_output::dump.txt"
