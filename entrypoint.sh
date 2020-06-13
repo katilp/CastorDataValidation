a7838fe9fe5fc5beef8cc5093b0056cb949b6289
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2020-06-13 17:14:02 +0200

    lowpupatch1 also in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,12 @@ cd /home/cmsusr
 
 set  -e
 
-echo "Setting up CMSSSW_4_2_8"
+echo "Setting up CMSSSW_4_2_8_lowpupatch1"
 source /opt/cms/cmsset_default.sh
-scramv1 project CMSSW CMSSW_4_2_8
-cd CMSSW_4_2_8/src
+scramv1 project CMSSW CMSSW_4_2_8_lowpupatch1
+cd CMSSW_4_2_8_lowpupatch1/src
 eval `scramv1 runtime -sh`
-echo "CMSSW_4_2_8 is at your service."
+echo "CMSSW_4_2_8_lowpupatch1 is at your service."
 
 
 git clone git://github.com/katilp/CastorDataValidation.git
