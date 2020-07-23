e01fb16dd0f8542eb810e1e58d5d3426fda7cc33
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2020-07-23 15:58:42 +0200

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -9,6 +9,20 @@ scram b
 
 cd CastorDataValidation/Commissioning10Analyzer/
 
+#change number of events to 10000 if not input or to input value if give
+if [ -z "$1" ]
+  then
+    nev=10000
+  else 
+    nev=$1
+fi
+dataeventline=$(grep maxEvents analyzer_cfg_Commissioning10.py)
+sed -i "s/$dataeventline/process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32($nev) )/g" analyzer_cfg_Commissioning10.py
+mceventline=$(grep maxEvents analyzer_cfg_Comm10MC.py)
+sed -i "s/$mceventline/process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32($nev) )/g" analyzer_cfg_Comm10MC.py
+
+
+
 cmsRun analyzer_cfg_Commissioning10.py
 cmsRun analyzer_cfg_Comm10MC.py
 
